87c21821411a238c3d5df62a966d744ccc28cb5e
joe bumbaca <joebumbaca@gmail.com>
2019-05-21 07:57:41 -0700

    Complete Mancala

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -26,21 +26,33 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
     @cups[start_pos] = []
-    start = (start_pos + 1) % 14
-    end_pos = (start_pos + 4) % 14
-    (start..end_pos).each do |i|
-      @cups[i] << :stone
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop 
+      end
     end
-    #render
-    next_turn(end_pos)
-    if @cups[end_pos].length == 1
-      return :switch
-    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else 
+      ending_cup_idx
+    end
   end
 
   def render
@@ -56,5 +68,13 @@ class Board
   end
 
   def winner
+    player1_total = @cups[6].length
+    player2_total = @cups[13].length
+
+    if player1_total == player2_total
+      :draw
+    else 
+      player1_total > player2_total ? @name1 : @name2
+    end
   end
 end
